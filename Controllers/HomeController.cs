a2b93a78016a3fb769153ab2ce35a96334189a59
HoangTran <99.hoangtran@gmail.com>
2020-12-04 23:27:04 +0700

    [Feature] Add Home basic view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,27 +1,51 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using TodoListMVC.Data;
+using TodoListMVC.Data.Entities;
 using TodoListMVC.Models;
+using TodoListMVC.Services;
 
 namespace TodoListMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly ICurrentUserService _currentUserService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context, ICurrentUserService currentUserService)
         {
-            _logger = logger;
+            _context = context;
+            _currentUserService = currentUserService;
         }
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            var privateTodoes = _context.Todos
+                .Where(t => t.CreatedBy == _currentUserService.UserId)
+                .ToList();
+
+            // https://stackoverflow.com/a/3547706
+            var assignedTodoes = _context.Todos
+                .Where(t => t.CreatedBy != _currentUserService.UserId)
+                .Join(_context.Assignments, t => t.Id, a => a.TodoId, (t, a) => new {t, a})
+                .Where(o => o.a.UserId == _currentUserService.UserId)
+                .Select(o => o.t)
+                .ToList();
+
+            var publicTodoes = _context.Todos
+                .Where(t => t.CreatedBy != _currentUserService.UserId && t.Scope == TodoScope.Public)
+                .ToList();
+
+            var homeViewModel = new HomeViewModel {
+                PrivateTodoes = privateTodoes,
+                AssignedTodoes = assignedTodoes,
+                PublicTodoes = publicTodoes
+            };
+
+            return View(homeViewModel);
         }
         [Authorize]
         public IActionResult Privacy()
---
+++ Models/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoListMVC.Data.Entities;
+using TodoListMVC.Shared;
+
+namespace TodoListMVC.Models
+{
+    public class HomeViewModel
+    {
+        public List<Todo> PrivateTodoes { get; set; }
+        public List<Todo> AssignedTodoes{ get; set; }
+        public List<Todo> PublicTodoes { get; set; }
+    }
+}
